ad0ffd3079490fc76b9909d64b455db610433922
e-lawniczak <60924751+e-lawniczak@users.noreply.github.com>
2026-06-04 04:06:48 +0200

    Update HomePage.tsx

--- Frontend/src/pages/home/HomePage.tsx
+++ Frontend/src/pages/home/HomePage.tsx
@@ -1,21 +1,12 @@
 import { BasePage } from "../../components/layout/BasePage"
 import './_home.scss'
-import { useState } from "react"
-import { APIClient } from "../../helpers/APIClient"
-import { API_ROUTES } from "../../components/common/API_ROUTES"
 
 export default () => {
-    const [value, setValue] = useState<any>(null);
-    const clickButton = async () => {
-        var v = new APIClient();
-        var res = await v.get(API_ROUTES.me)
-        setValue(res.data)
-    }
+
+
     return <BasePage pageCssClass="home-page">
         <div className="wrapper">
             Home Page
         </div>
-        <button onClick={clickButton}>Click me</button>
-        <div>{value?.email}</div>
     </BasePage>
 }
